fb141a8913352f3bfddb5663e936f1cb68815745
Talha <noname_2338@hotmail.com>
2020-10-06 20:16:02 +0200

    added dog age calculated

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let myAge = 19
+//defined my age
+let earlyYears = 2
+//first 2 years
+ earlyYears = earlyYears*10.5;
+ let laterYears = myAge - 2;
+ //result age
+ laterYears = laterYears*4;
+//later years defined
+let myAgeInDogYears = earlyYears + laterYears
+//calculated total age
+let myName = 'Talha'
+console.log(myName.toLowerCase())
+//defined my name
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+//defined almost everything
+
+
